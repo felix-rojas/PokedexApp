30f9eda014f15506e7974bfcc52d7aea168ea257
felix-rojas <a01201946@tec.mx>
2023-03-07 08:30:55 -0600

    add API service

---
+++ app/src/main/java/com/example/mypokedexapp/PokemonAPIService.kt
@@ -0,0 +1,21 @@
+package com.example.mypokedexapp
+
+import com.example.mypokedexapp.pokemon.Pokemon
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PokemonAPIService {
+    //https://pokeapi.co/api/v2/pokemon/?limit=1279
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("limit") limit:Int
+    ): PokedexObject
+
+    //https://pokeapi.co/api/v2/pokemon/{number_pokemon}/
+    @GET("pokemon/{numberPokemon}")
+    suspend fun getPokemonInfo(
+        @Path("numberPokemon") numberPokemon:Int
+    ): Pokemon
+}
+
